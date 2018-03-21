aab5ad7840f77053858c121c4ed499a6dd24481e
SanzenSekai1001 <santoryu1001@gmail.com>
2018-03-21 05:23:12 +0300

    create full Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,17 @@
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int summ = first + second;
+		int razn = first - second;
+		int proizv = first * second;
+		float chast = Float.valueOf(first) / Float.valueOf(second);
+		int step = (int)Math.pow(first, second);
+		System.out.println("Summ = " + summ);
+		System.out.println("Razn = " + razn);
+		System.out.println("Proizv = " + proizv);
+		System.out.println("Chast = " + chast);
+		System.out.println("Step = " + step);
 	}
 }
